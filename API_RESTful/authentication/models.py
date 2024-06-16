509965f22ca0236cfba4434666406f9a3a844f0d
Thorrien <eric.bariller.49@gmail.com>
2024-06-16 21:50:29 +0200

    Mise en place d'un User et d'un contributor. Des modèles Projetc, Issue et Comment.

--- API_RESTful/authentication/models.py
+++ API_RESTful/authentication/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    age = models.IntegerField()
+    can_be_contacted = models.BooleanField(default=False)
+    can_be_shared = models.BooleanField(default=False)
+
+
+class Contributor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
--- API_RESTful/core/models.py
+++ API_RESTful/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
+from authentication.models import Contributor
 
-# Create your models here.
+class Project(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    contributors = models.ManyToManyField(Contributor, related_name='Contributeurs', blank=True)
+
+
+class Issue(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
